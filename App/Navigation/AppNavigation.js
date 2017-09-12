b731307363476b23a0f289ddb1c2d241168cc177
Tahmid Shakil <at.shakil.92@gmail.com>
2017-09-12 20:56:03 +0600

    Screens

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import AboutScreen from '../Containers/AboutScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  AboutScreen: { screen: AboutScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
